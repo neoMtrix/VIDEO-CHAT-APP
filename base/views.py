e1538f5eca5e15e03da2bf03d93817ad7310446a
Ernel Tortogo <ernel.tortogo@mobilemoney.ph>
2022-12-26 14:11:14 +0800

    version 0

---
+++ base/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random
+import time
+import json
+
+from .models import RoomMember
+
+from django.views.decorators.csrf import csrf_exempt
+
+def getToken(request):
+    appId = '31b7d09312d642f88d8a0f6c99f190c6'
+    appCertificate = 'efc81382036c43c3b5810535435f1935'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1,230)
+    expirationTimeSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token':token, 'uid':uid}, safe=False)
+
+def lobby(request):
+    return render(request, "base/lobby.html")
+
+
+def room(request):
+    return render(request, "base/room.html")
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+    return JsonResponse({'name':data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+
+    name = member.name
+    return JsonResponse({'name':member.name}, safe=False)
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+
+    member = RoomMember.objects.get(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name'],
+    )
+    member.delete()
+    return JsonResponse('Member was deleted', safe=False) 
